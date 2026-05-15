b9510291ae48b8c279bc6ce81ae1bc3137216db5
Jose Prendergast <150608797+joseprendergast@users.noreply.github.com>
2026-05-14 21:41:03 -0400

    Add drifter id constants

---
+++ Unity/TheLastDrifter/Assets/TheLastDrifter/Scripts/Core/DrifterIds.cs
@@ -0,0 +1,16 @@
+namespace TheLastDrifter.Core
+{
+    public static class DrifterIds
+    {
+        public const string Alley = "alley";
+        public const string BloodLab = "blood_lab";
+        public const string Zoo = "zoo";
+
+        public const string SawHand = "saw_hand";
+        public const string CheckedDrain = "checked_drain";
+        public const string LabPowerRestored = "lab_power_restored";
+        public const string RecoveredMemoryReel = "recovered_memory_reel";
+        public const string SawBleedingSign = "saw_bleeding_sign";
+        public const string HeardChildWarning = "heard_child_warning";
+    }
+}
